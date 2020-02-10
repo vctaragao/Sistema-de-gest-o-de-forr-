aa5dc8f40744b669b814ee35d2d5e237b61f3539
Victor Moraes de Aragão <vct.aragao@unifei.edu.br>
2020-02-10 19:39:09 -0300

    Arquivo de configuração Laravel Envoy

---
+++ Envoy.blade.php
@@ -0,0 +1,60 @@
+@servers(['web' => 'deployer@206.189.216.247'])
+
+@setup
+$repository = 'git@gitlab.com:vct.aragao/Sistema-de-gestao-forro.git';
+$releases_dir = '/var/www/sigf/releases';
+$app_dir = '/var/www/sigf';
+$release = date('YmdHis');
+$new_release_dir = $releases_dir .'/'. $release;
+@endsetup
+
+@story('deploy')
+setup_app_dir
+clone_repository
+run_composer
+create_storage_and_env
+create_links
+@endstory
+
+@task('setup_app_dir')
+echo 'Checking app dir'
+[ -d {{ $app_dir }} ] && echo "Exists" || mkdir {{ $app_dir }}
+@endtask
+
+@task('clone_repository')
+echo 'Cloning repository'
+[ -d {{ $releases_dir }} ] || mkdir {{ $releases_dir }}
+git clone --depth 1 {{ $repository }} {{ $new_release_dir }}
+cd {{ $new_release_dir }}
+git reset --hard {{ $commit }}
+@endtask
+
+@task('run_composer')
+echo "Starting deployment ({{ $release }})"
+cd {{ $new_release_dir }}
+composer install --prefer-dist --no-scripts -q -o
+@endtask
+
+@task('create_storage_and_env');
+echo "Creating storage folder and It's sub folders"
+[ -d {{ $new_release_dir }}/storage ] && (rm -rf {{ $new_release_dir }}/storage && mkdir {{ $new_release_dir }}/storage)
+|| mkdir {{ $new_release_dir }}/storage
+mkdir {{ $new_relase_dir }}/storage/frameworks/cache
+mkdir {{ $new_relase_dir }}/storage/frameworks/sessions
+mkdir {{ $new_relase_dir }}/storage/frameworks/views
+
+echo "Creating .env file"
+[ -f {{ $new_release_dir }}/.env ] && (rm {{ $new_release_dir }}/.env && touch {{ $new_release_dir }}/.env) || touch
+{{ $new_release_dir }}/.env
+
+@task('create_links')
+echo "Linking storage directory"
+rm -rf {{ $new_release_dir }}/storage
+ln -nfs {{ $app_dir }}/storage {{ $new_release_dir }}/storage
+
+echo 'Linking .env file'
+ln -nfs {{ $app_dir }}/.env {{ $new_release_dir }}/.env
+
+echo 'Linking current release'
+ln -nfs {{ $new_release_dir }} {{ $app_dir }}/current
+@endtask
